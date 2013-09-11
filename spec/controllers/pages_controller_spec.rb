834eebb9b803fa83a4cdb78d7a65db60dfcd4540
Jerome Barbet <mule.from.space@gmail.com>
2013-09-11 15:52:56 +0200

    amélioration des controles sur les titres

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
+
+  before(:each) do
+    @base_titre = "SampleApp du tutoriel Ruby on Rails"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    it "should have the good title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_titre + " | Home")
     end
   end
 
@@ -14,6 +24,11 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the good title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_titre + " | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -21,12 +36,22 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    it "should have the good title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_titre + " | About")
+    end
   end
 
   describe "GET 'help'" do
     it "should be successful" do
       get 'help'
       response.should be_success
+    end
+    it "should have the good title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => @base_titre + " | Help")
     end
     
   end
