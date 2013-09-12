080c9ca96dc37e8024868ee65b2f6661a3f5c3c2
Jerome Barbet <mule.from.space@gmail.com>
2013-09-12 19:07:54 +0200

    Ajout du framework css blueprint

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+	def titre
+		base_titre = "SampleApp du tutoriel Ruby on Rails"
+		if @titre.nil?
+			base_titre
+		else
+			"#{base_titre} | #{@titre}"
+		end
+		
+	end
+
 end
