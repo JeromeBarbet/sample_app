7ac17b12c1ceafe7c18098c03922fcc36cb9d365
Jerome Barbet <mule.from.space@gmail.com>
2013-09-12 23:01:15 +0200

    Suppression de la page index par defaut de rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/help', :to => 'pages#help'
+  match '/about', :to => 'pages#about'
+
   get "pages/home"
 
   get "pages/contact"
@@ -56,7 +60,7 @@ SampleApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "Pages#home"
 
   # See how all your routes lay out with "rake routes"
 
